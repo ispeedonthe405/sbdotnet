529d9d879307e3f19b722037a8f6b61c4fd2093a
ispeedonthe405 <ispeedonthe405@gmail.com>
2025-02-13 09:20:46 -0800

    New method for retrieving an assembly resource as a string for further processing (e.g. json, js, etc)

--- sbdotnet/AssemblyUtils.cs
+++ sbdotnet/AssemblyUtils.cs
@@ -4,6 +4,31 @@ namespace sbdotnet
 {
     public class AssemblyUtils
     {
+        public static string LoadResourceString(string path)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                using (var resource = Assembly.GetCallingAssembly().GetManifestResourceStream(path))
+                {
+                    if (resource is not null)
+                    {
+                        using (var stream = new StreamReader(resource))
+                        {
+                            result = stream.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+
+            return result;
+        }
+
         public static List<string> GetResourceNames(string prefix, string suffix)
         {
             List<string> names = [];
