f76802c888a3a8d73bbb8907fb725d40253b2474
ispeedonthe405 <ispeedonthe405@gmail.com>
2025-03-28 19:54:26 -0700

    counting digits

--- sbdotnet/Extensions.cs
+++ sbdotnet/Extensions.cs
@@ -10,6 +10,43 @@ namespace sbdotnet
     public static class Extensions
     {
         /////////////////////////////////////////////////////////
+        #region rgname
+
+        public static int DigitCount(this int n)
+        {
+            if (n >= 0)
+            {
+                if (n < 10) return 1;
+                if (n < 100) return 2;
+                if (n < 1000) return 3;
+                if (n < 10000) return 4;
+                if (n < 100000) return 5;
+                if (n < 1000000) return 6;
+                if (n < 10000000) return 7;
+                if (n < 100000000) return 8;
+                if (n < 1000000000) return 9;
+                return 10;
+            }
+            else
+            {
+                if (n > -10) return 2;
+                if (n > -100) return 3;
+                if (n > -1000) return 4;
+                if (n > -10000) return 5;
+                if (n > -100000) return 6;
+                if (n > -1000000) return 7;
+                if (n > -10000000) return 8;
+                if (n > -100000000) return 9;
+                if (n > -1000000000) return 10;
+                return 11;
+            }
+        }
+
+        #endregion rgname
+        /////////////////////////////////////////////////////////
+
+
+        /////////////////////////////////////////////////////////
         #region DateTime
 
         public static long ToNixTimestamp_S(this DateTime dt)
